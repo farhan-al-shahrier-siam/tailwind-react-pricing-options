6a901693f55b4be00872d5bfe088fcbd080c8e20
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-17 22:18:50 +0600

    Added the navbar as my own design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import { CircleX, Menu } from "lucide-react";
 
 const navigationData = [
     {
@@ -30,13 +31,23 @@ const navigationData = [
 ];
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+
+    const links = navigationData.map((route) => <Link key={route.id} route={route}></Link>);
+
     return (
-        <nav>
-            <ul className="flex">
-                {navigationData.map((route) => (
-                    <Link key={route.id} route={route}></Link>
-                ))}
-            </ul>
+        <nav className="flex justify-between items-center mx-6 mt-4">
+            <span onClick={() => setOpen(!open)} className="flex items-center">
+                {open ? <CircleX className="md:hidden"></CircleX> : <Menu className="md:hidden"></Menu>}
+
+                {
+                    <ul className={`md:hidden absolute duration-1000 bg-amber-300 p-2 rounded-2xl z-10 text-black ${open ? 'top-13' : '-top-40'}`}>
+                        {links}
+                    </ul>
+                }
+                <h3 className="ml-4 btn btn-ghost text-xl">My Navbar</h3>
+            </span>
+            <ul className="hidden md:flex">{links}</ul>
 
             {/* <ul className="flex"> 
                 {
@@ -49,6 +60,7 @@ const NavBar = () => {
                 <li className='mr-10 '><a href="/about">About</a></li>
                 <li className='mr-10 '><a href="/blog">Blog</a></li>
             </ul> */}
+            <button className="btn">Sign In</button>
         </nav>
     );
 };
